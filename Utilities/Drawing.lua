e4a73e8a26868e241d1a943d755412a2160d8c43
cl0wnfoon <87553151+cl0wnfoon@users.noreply.github.com>
2024-07-07 23:41:39 -0500

    Update Drawing.lua

--- Utilities/Drawing.lua
+++ Utilities/Drawing.lua
@@ -1,5 +1,3 @@
-task.wait(10)
-
 local UserInputService = game:GetService("UserInputService")
 local InsertService = game:GetService("InsertService")
 local RunService = game:GetService("RunService")
@@ -239,7 +237,7 @@ elseif game.GameId == 358276974 or game.GameId == 3495983524 then -- Apocalypse 
     -- TODO: Squad GetTeam function
     --function GetTeam(Target, Character, Mode) end
 elseif game.GameId == 1054526971 then -- Blackhawk Rescue Mission 5
-    --[[local function RequireModule(Name)
+    local function RequireModule(Name)
         for Index, Instance in pairs(getmodules()) do
             if Instance.Name == Name then
                 return require(Instance)
@@ -266,7 +264,7 @@ elseif game.GameId == 1054526971 then -- Blackhawk Rescue Mission 5
         else
             return not FindFirstChildWhichIsA(Character, "ProximityPrompt", true), WhiteColor
         end
-    end]]
+    end
 elseif game.GameId == 580765040 then -- RAGDOLL UNIVERSE
     function GetCharacter(Target, Mode)
         local Character = Target.Character
